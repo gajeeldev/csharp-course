bc75fb5f60059fbec5ed1a87d54b6b962a99b7e4
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-12-08 13:13:45 -0400

    update

--- Backend/Backend/Controllers/PeopleController.cs
+++ Backend/Backend/Controllers/PeopleController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Backend.Services;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Backend.Controllers
@@ -7,14 +8,44 @@ namespace Backend.Controllers
     [ApiController]
     public class PeopleController : ControllerBase
     {
+        private IPeopleService _peopleService;
+
+        public PeopleController([FromKeyedServices("people2Service")]IPeopleService peopleService)
+        {
+            _peopleService = peopleService;
+        }
+
         [HttpGet("all")]
         public List<People> GetPeople() => Repository.People;
 
         [HttpGet("{id}")]
-        public People Get(int id) => Repository.People.First(p => p.Id == id);
+        public ActionResult<People> Get(int id)
+        {
+            var people = Repository.People.FirstOrDefault(p => p.Id == id);
+
+            if (people == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(people);
+        }
+
         [HttpGet("search/{search}")]
         public List<People> Get(string search) =>
             Repository.People.Where(p => p.Name.ToUpper().Contains(search.ToUpper())).ToList();
+        [HttpPost]
+        public IActionResult Add(People people)
+        {
+            if (!_peopleService.Validate(people))
+            {
+                return BadRequest();
+            }
+
+            Repository.People.Add(people);
+
+            return NoContent();
+        }
 
     }
 
--- Backend/Backend/Program.cs
+++ Backend/Backend/Program.cs
@@ -1,6 +1,15 @@
+using Backend.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+//builder.Services.AddSingleton<IPeopleService, PeopleService>();
+builder.Services.AddKeyedSingleton<IPeopleService, PeopleService>("peopleService");
+builder.Services.AddKeyedSingleton<IPeopleService, People2Service>("people2Service");
+
+builder.Services.AddKeyedSingleton<IRandomService, RandomService>("randomSingleton");
+builder.Services.AddKeyedScoped<IRandomService, RandomService>("randomScoped");
+builder.Services.AddKeyedTransient<IRandomService, RandomService>("randomTransient");
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
