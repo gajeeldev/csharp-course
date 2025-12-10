dba6d2ec09c2c045fc84ce72f8a29964815b4628
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-12-10 16:44:05 -0400

    Create SomeController.cs

---
+++ Backend/Backend/Controllers/SomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SomeController : ControllerBase
+    {
+        [HttpGet("sync")]
+        public IActionResult GetSync()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Conexion a base de datos terminada");
+
+            Thread.Sleep(1000);
+            Console.WriteLine("Envio de email terminado");
+
+            Console.WriteLine("Proceso terminado");
+
+            stopwatch.Stop();
+            return Ok(stopwatch.Elapsed);
+        }
+
+        [HttpGet("async")]
+        public async Task<IActionResult> GetAsync()
+        {
+            Stopwatch stopwatch = Stopwatch.StartNew();
+            stopwatch.Start();
+
+            var task1 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Conexion a base de datos terminada");
+                return 1;
+            });
+
+            var task2 = new Task<int>(() =>
+            {
+                Thread.Sleep(1000);
+                Console.WriteLine("Envio de email terminado");
+                return 2;
+            });
+
+            task1.Start();
+            task2.Start();
+
+            Console.WriteLine("Hago otra cosa");
+
+            var result1 = await task1;
+            var result2 = await task2;
+            Console.WriteLine("Proceso terminado");
+
+            stopwatch.Stop();
+            return Ok($"{result1} - {result2} / {stopwatch.Elapsed}");
+        }
+    }
+}
