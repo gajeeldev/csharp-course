d1b667467d582ddf58126170303aefe046dcfb3a
Gilbert Acosta <155097224+gajeeldev@users.noreply.github.com>
2025-12-11 10:44:07 -0400

    update

---
+++ Backend/Backend/Controllers/PostsController.cs
@@ -0,0 +1,19 @@
+﻿using Backend.DTOs;
+using Backend.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PostsController : ControllerBase
+    {
+        IPostsService _postsService;
+        public PostsController(IPostsService postsService) => _postsService = postsService;
+
+
+        [HttpGet("all")]
+        public async Task<IEnumerable<PostDto>> Get() => await _postsService.Get();
+    }
+}
---
+++ Backend/Backend/Services/PostsService.cs
@@ -0,0 +1,31 @@
+﻿using Backend.DTOs;
+using System.Text.Json;
+
+namespace Backend.Services
+{
+    public class PostsService : IPostsService
+    {
+
+        private HttpClient _httpClient;
+        public PostsService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<IEnumerable<PostDto>> Get()
+        {
+          
+            var result = await _httpClient.GetAsync(_httpClient.BaseAddress + "/posts");
+            var body = await result.Content.ReadAsStringAsync();
+
+            var options = new JsonSerializerOptions
+            {
+                PropertyNameCaseInsensitive = true
+            };
+
+            var posts = JsonSerializer.Deserialize<IEnumerable<PostDto>>(body, options);
+
+            return posts;
+        }
+    }
+}
